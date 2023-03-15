382552c8cfc215eeddb56548ab272adc429b4114
Shivani21-hude <shivani21hude@gmail.com>
2023-03-15 22:47:40 +0530

    [Shivani]-Problem basis on Class and object

---
+++ OopsConcepts/ObjectAndClassConcept.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcepts
+{
+    public class ObjectAndClassConcept
+    {
+        public void Addition()
+        {
+            int a = 12;
+            int b = 23;
+            Console.WriteLine("a+b : {0}",a+b);
+        }
+        public static void Substraction()
+        {
+            int a = 89;
+            int b = 43;
+            Console.WriteLine("a-b : {0}",a-b);
+        }
+    }
+}
---
+++ OopsConcepts/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcepts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ***** Welcome to Oops Concepts problems ***** ");
+            //Create a object of class ObjectAndClassConcept
+            ObjectAndClassConcept obj = new ObjectAndClassConcept();
+            Console.WriteLine("Choose given option : \n1)Class and opbject concept ");
+            int option=Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    //call the method
+                    obj.Addition();
+                    ObjectAndClassConcept.Substraction();
+                    break;
+            }
+        }
+    }
+}
